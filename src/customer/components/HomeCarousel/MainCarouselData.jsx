213ea1945cb698a4ca0a222ea090118b37469ce1
GaneshPatil1704 <pyogita868@gmail.com>
2024-08-18 11:15:11 +0530

    commit

---
+++ src/customer/components/HomeCarousel/MainCarouselData.jsx
@@ -0,0 +1,22 @@
+import photo1 from './Images/photo1.jpg';
+import photo2 from './Images/photo2.jpg';
+import photo3 from './Images/photo3.mp4';
+import photo4 from './Images/photo4.jpg';
+
+export const mainCarouselData = [{
+        image: photo1, // Corrected
+        path: "/women/clothing/lengha_choll"
+    },
+    {
+        image: photo2, // Corrected
+        path: "/women/clothing/women_dress"
+    },
+    {
+        image: photo3, // Corrected
+        path: "/women/clothing/women_dress"
+    },
+    {
+        image: photo4, // Corrected
+        path: "/women/clothing/women_saree"
+    }
+];
